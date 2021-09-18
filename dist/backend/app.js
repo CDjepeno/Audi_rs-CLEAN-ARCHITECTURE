b6609a17920362f12b2cc5d586108d0c4cf72cbd
CDjepeno <djepeno.christopher@gmail.com>
2021-09-18 15:02:06 +0200

    change configuration

--- dist/backend/app.js
+++ dist/backend/app.js
@@ -4,8 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const config_1 = __importDefault(require("./config/config"));
 const app = express_1.default();
+// app.use(bodyParser.urlencoded({extend: false}))
+app.use(body_parser_1.default.json());
 app.get('/', (req, res) => {
-    res.send('hello');
+    res.send('hello world');
 });
-app.listen(5000, () => console.log('Serveur running'));
+app.listen(config_1.default.server, () => console.log(`Serveur running ${config_1.default.server.port}`));
