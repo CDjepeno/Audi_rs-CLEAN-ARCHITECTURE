ced2ecdf4816ec74ffd9ba4630e9209860b7bf66
CDjepeno <djepeno.christopher@gmail.com>
2021-09-18 19:09:01 +0200

    change middleware

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -5,7 +5,7 @@ import config  from './config/config'
 const app: express.Application = express()
 
 // app.use(bodyParser.urlencoded({extend: false}))
-app.use(bodyParser.json())
+app.use(express.json())
 
 app.get('/', (req , res) => {
     res.send('hello world')
