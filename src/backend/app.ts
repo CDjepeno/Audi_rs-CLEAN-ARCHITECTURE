0e3408e51336703167e07a3c9d5fb131671b6df5
CDjepeno <djepeno.christopher@gmail.com>
2021-09-19 16:23:14 +0200

    change app.js

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -3,18 +3,17 @@ import config  from './config/config'
 import mongoose from "mongoose";
 
 (async () => {
-    await mongoose.connect(config.DB.uri,() => {
-        console.log('connected to database')
-    })
-
     const app: express.Application = express()
-
-    app.use(express.json())
-
+    app.listen(config.server.port, () => console.log(`Serveur running ${config.server.port}`))
 
     app.get('/', (req , res) => {
         res.send('hello world')
     })
 
-    app.listen(config.server.port, () => console.log(`Serveur running ${config.server.port}`))
+    await mongoose.connect(config.DB.uri,() => {
+        console.log('connected to database')
+    })
+
+    app.use(express.json())
+
 })()
