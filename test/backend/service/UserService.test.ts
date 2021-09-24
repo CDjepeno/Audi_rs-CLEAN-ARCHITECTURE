25cde4c9389de78da3973d76eccf623e83c6bbfe
CDjepeno <djepeno.christopher@gmail.com>
2021-09-24 13:47:16 +0200

    Change userServiceTest

--- test/backend/service/UserService.test.ts
+++ test/backend/service/UserService.test.ts
@@ -10,10 +10,10 @@ dotenv.config()
 
 describe('Test UserService module', () => {
     let mongoClient;
-    beforeAll(async () => {
-        console.log(process.env.MONGO_URI)
-        mongoClient = await connectDb(process.env.MONGO_URI_TEST)
-    })
+    // beforeAll(async () => {
+    //     console.log(process.env.MONGO_URI)
+    //     mongoClient = await connectDb(process.env.MONGO_URI_TEST)
+    // })
     it('createUser Test', async() => {
         const user = UserRepositoryBuilder.userStub()
         const userService = new UserService()
