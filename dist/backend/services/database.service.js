0aa95890530a4b9dc2f28528c5ed4ccea8ee334e
CDjepeno <djepeno.christopher@gmail.com>
2021-09-24 13:39:02 +0200

    fix databaseConexion

--- dist/backend/services/database.service.js
+++ dist/backend/services/database.service.js
@@ -1,26 +1,11 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectToDatabase = void 0;
-const config_1 = __importDefault(require("../config/config"));
-const mongodb_1 = require("mongodb");
-function connectToDatabase() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const client = new mongodb_1.MongoClient(config_1.default.DB.uri);
-        yield client.connect();
-        const db = client.db(config_1.default.DB.name);
-        console.log(`Successfully connected to database: ${db.databaseName}`);
-    });
-}
-exports.connectToDatabase = connectToDatabase;
+// export async function connectToDatabase() {
+//     const client: mongoDB.MongoClient = new MongoClient(config.DB.uri)
+//
+//     await client.connect()
+//
+//     const db: mongoDB.Db = client.db(config.DB.name)
+//
+//     console.log(`Successfully connected to database: ${db.databaseName}`)
+// }
