c7efad1cbc24557c8f92a2daca63876c0e621133
CDjepeno <djepeno.christopher@gmail.com>
2021-09-24 15:06:50 +0200

    Change userServiceTest

--- test/backend/service/UserService.test.ts
+++ test/backend/service/UserService.test.ts
@@ -1,24 +1,25 @@
 // @ts-ignore
 import {UserRepositoryBuilder} from "../../builder/UserRepositoryBuilder";
-import request from 'supertest'
-import app from "../../../src/backend/app";
 import { UserService } from "../../../src/backend/services/userService";
 import {connectDb} from "../../../src/backend/database/mongodb";
 import dotenv from 'dotenv'
+import mongoose from "mongoose";
 
 dotenv.config()
 
+jest.setTimeout(60000)
 describe('Test UserService module', () => {
     let mongoClient;
-    // beforeAll(async () => {
-    //     console.log(process.env.MONGO_URI)
-    //     mongoClient = await connectDb(process.env.MONGO_URI_TEST)
-    // })
+    beforeAll(async () => {
+        mongoClient = await connectDb(process.env.MONGO_URI_TEST)
+    })
     it('createUser Test', async() => {
+
         const user = UserRepositoryBuilder.userStub()
         const userService = new UserService()
         const result = await userService.addUser(user)
 
         expect(result).toBe('Utilisateur ajouté')
     })
+    // afterAll(async() => await mongoose.connection.db.dropCollection("users"))
 })
--- test/database/mongodbtest.ts
+++ test/database/mongodbtest.ts
@@ -5,19 +5,19 @@ import {connectDb} from "../../src/backend/database/mongodb";
 describe('Mongodb Service', () => {
     let mongoClient;
 
-    beforeAll(async () => {
-        mongoClient = await connectDb(process.env.MONGO_URI_TEST)
-    })
+    // beforeAll(async () => {
+    //     mongoClient = await connectDb(process.env.MONGO_URI_TEST)
+    // })
 
     // it("create user", async() => {
     //     await
     // })
 
-    afterEach(async () => {
-        await mongoClient.connection.db.dropDatabase()
-    })
-
-    afterAll(async () => {
-        await mongoClient.connection.close()
-    })
+    // afterEach(async () => {
+    //     await mongoClient.connection.db.dropDatabase()
+    // })
+    //
+    // afterAll(async () => {
+    //     await mongoClient.connection.close()
+    // })
 })
